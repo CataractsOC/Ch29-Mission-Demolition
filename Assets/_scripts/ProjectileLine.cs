95e91f2156fe7bad3db9c8f74e51971fc55fd845
JFU5 <JFU5@CILABS-114B-10.scilsnet.rutgers.edu>
2018-06-19 19:59:57 -0400

    saving work

--- Assets/_scripts/ProjectileLine.cs
+++ Assets/_scripts/ProjectileLine.cs
@@ -3,9 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileLine : MonoBehaviour {
+    static public ProjectileLine S;
 
-	// Use this for initialization
-	void Start () {
+    [Header("Set in Inspector")]
+    public float minDist = 0.1f;
+
+    private LineRenderer line;
+    private GameObject _poi;
+    private List<Vector3> points;
+
+    void Awake() {
+        S = this;
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+        points = new List<Vector3>();
+    }
+
+    public GameObject poi {
+        get {
+            return (_poi);
+        }
+        set {
+            _poi = value;
+            if (_poi != null) {
+                line.enabled = false;
+                points = new List<Vector3>();
+                AddPoint();
+            }
+        }
+    }
+
+    public void Clear() {
+        _poi = null;
+        line.enabled = false;
+        points = new List<Vector3>();
+    }
+
+    public void AddPoint() {
+        Vector3 pt = _poi.transform.position;
+        if (points.Count > 0 && (pt - lastPoint).magnitude < minDist) {
+            return;
+        }
+        if (pointsCount == 0 )
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
