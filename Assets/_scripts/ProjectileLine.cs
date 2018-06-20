6b7c8bd8756e7e278591483b0dc082a4e8f6fb62
Josh Felipe Ursua <joshursua@airmail.cc>
2018-06-20 01:59:53 -0400

    Added projectile line, goal changing script, and some extra castles.
    Will add more in the morning. Exhausted   currently.

--- Assets/_scripts/ProjectileLine.cs
+++ Assets/_scripts/ProjectileLine.cs
@@ -44,16 +44,48 @@ public class ProjectileLine : MonoBehaviour {
         if (points.Count > 0 && (pt - lastPoint).magnitude < minDist) {
             return;
         }
-        if (pointsCount == 0 )
+        if (points.Count == 0) {
+            Vector3 launchPosDiff = pt - Slingshot.LAUNCH_POS;
+            points.Add(pt + launchPosDiff);
+            points.Add(pt);
+            line.positionCount = 2;
+            line.SetPosition(0, points[0]);
+            line.SetPosition(1, points[1]);
+            line.enabled = true;
+        } else {
+            points.Add(pt);
+            line.positionCount = points.Count;
+            line.SetPosition(points.Count - 1, lastPoint);
+            line.enabled = true;
+        }
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector3 lastPoint {
+        get {
+            if (points == null) {
+                return (Vector3.zero);
+            }
+            return (points[points.Count - 1]);
+        }
+    }
+
+    void FixedUpdate() {
+        if (poi == null) {
+            if (FollowCam.POI != null) {
+                if (FollowCam.POI.tag == "Projectile") {
+                    poi = FollowCam.POI;
+                }
+                else {
+                    return;
+                }
+            }
+            else {
+                return;
+            }
+        }
+        AddPoint();
+        if (FollowCam.POI == null) {
+            poi = null;
+        }
+    }
 }
--- Assets/_scripts/Slingshot.cs
+++ Assets/_scripts/Slingshot.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -14,8 +15,16 @@ public class Slingshot : MonoBehaviour {
 
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS {
+        get {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake () {
-        Transform launchPointTrans = transform.Find("LaunchPoint");
+        S = this;
+        Transform launchPointTrans = transform.FindChild("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
         launchPos = launchPointTrans.position;
